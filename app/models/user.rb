feed40c3fe1f21176a8022d2791dc3afbfd8776e
Nikolay Bekirov <Nikolay.Bekirov@emerchantpay.com>
2016-02-22 16:38:49 +0200

    Set default User role after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   rolify
   belongs_to :organisation
+
+  after_create :set_default_role
+
+  protected
+
+  def set_default_role
+    self.add_role :default
+  end
 end
